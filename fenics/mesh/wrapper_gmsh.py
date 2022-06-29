90ff34b1136e49a2637fb9a1116e2094f96220f7
Felipe <felipemech@gmail.com>
2022-06-29 16:04:40 +0200

    changes in fenics io

--- fenics/mesh/wrapper_gmsh.py
+++ fenics/mesh/wrapper_gmsh.py
@@ -1,3 +1,9 @@
+# ============================================================================= 
+# The class Gmsh is wrapper for pygmsh Geometry. It provides basic functionalities
+# to interect with meshio and fenics. 
+# 
+# =============================================================================
+
 import numpy as np
 import meshio
 import pygmsh
@@ -8,70 +14,68 @@ from functools import reduce
 from fetricks.fenics.postprocessing.wrapper_io import exportMeshHDF5_fromGMSH
 from fetricks.fenics.mesh.mesh import Mesh
 
+
 class Gmsh(pygmsh.built_in.Geometry):
-    def __init__(self):
+    def __init__(self, meshname = "default.xdmf"):
         super().__init__()   
         self.mesh = None
-        self.radFileMesh = 'defaultMesh.{0}'
-        self.format = 'xdmf'
+        self.setNameMesh(meshname)
+        self.gmsh_opt = '-algo front2d -smooth 2 -anisoMax 1000.0'
         
-    def writeGeo(self, savefile = ''):
-        if(len(savefile) == 0):
-            savefile = self.radFileMesh.format('geo')
-            
+    # write .geo file necessary to produce msh files using gmsh
+    def writeGeo(self):
+        savefile = self.radFileMesh.format('geo')
         f = open(savefile,'w')
         f.write(self.get_code())
         f.close()
 
-    def writeXML(self, savefile = ''):
-        if(len(savefile) == 0):
-            savefile = self.radFileMesh.format('xml')
-        else:
-            self.radFileMesh = savefile[:-4] + '.{0}'
-         
+    # write .xml files (standard for fenics): 
+    def writeXML(self):
+        meshXMLFile = self.radFileMesh.format('xml')
+        meshMshFile = self.radFileMesh.format('msh')
+        self.writeMSH()
+        
+        os.system('dolfin-convert {0} {1}'.format(meshMshFile, meshXMLFile))    
+    
+    def writeMSH(self, gmsh_opt = ''):
+        self.writeGeo()
         meshGeoFile = self.radFileMesh.format('geo')
         meshMshFile = self.radFileMesh.format('msh')
-        self.writeGeo(meshGeoFile)
-        os.system('gmsh -2 -format msh2 -algo del2d' + meshGeoFile) # with del2d, noticed less distortions
-        os.system('dolfin-convert {0} {1}'.format(meshMshFile, savefile))    
     
-    def write(self,savefile = '', opt = 'meshio'):
-        if(type(self.mesh) == type(None)):
-            # self.generate(gmsh_opt=['-bin','-v','1', '-algo', 'del2d']) # with del2d, noticed less distortions      
-            self.generate(gmsh_opt=['-bin','-v','1', '-algo', 'front2d', 
-                                    '-smooth', '2',  '-anisoMax', '1000.0']) # with del2d, noticed less distortions      
-        if(len(savefile) == 0):
-            savefile = self.radFileMesh.format('xdmf')
+        os.system('gmsh -2 -format msh2 {0} {1} -o {2}'.format(self.gmsh_opt, meshGeoFile, meshMshFile))  # with del2d, noticed less distortions
         
+        self.mesh = meshio.read(meshMshFile)
+        
+    def write(self, opt = 'meshio'):
         if(opt == 'meshio'):
+            savefile = self.radFileMesh.format('msh')
             meshio.write(savefile, self.mesh)
         elif(opt == 'fenics'):
+            savefile = self.radFileMesh.format('xdmf')
             exportMeshHDF5_fromGMSH(self.mesh, savefile)
-        
-        # return self.mesh
-
             
-    def generate(self , gmsh_opt = ['']):
-        self.mesh = pygmsh.generate_mesh(self, extra_gmsh_arguments = gmsh_opt, dim = 2,mesh_file_type = 'msh2') # it should be msh2 cause of tags    
-        # self.mesh = pygmsh.generate_mesh(self, verbose=False, dim=2, prune_vertices=True, prune_z_0=True,
-                                          # remove_faces=False, extra_gmsh_arguments=gmsh_opt,  mesh_file_type='msh4') # it should be msh2 cause of tags
+    def generate(self):
+        self.mesh = pygmsh.generate_mesh(self, extra_gmsh_arguments = self.gmsh_opt.split(), dim = 2, mesh_file_type = 'msh2') # it should be msh2 cause of tags    
+                                          
 
-    def getEnrichedMesh(self, savefile = ''):
+    # def getEnrichedMesh(self, savefile = ''):
         
-        if(len(savefile) == 0):
-            savefile = self.radFileMesh.format(self.format)
+    #     if(len(savefile) == 0):
+    #         savefile = self.radFileMesh.format(self.format)
         
-        if(savefile[-3:] == 'xml'):
-            self.writeXML(savefile)
+    #     if(savefile[-3:] == 'xml'):
+    #         self.writeXML(savefile)
             
-        elif(savefile[-4:] == 'xdmf'):
-            print("exporting to fenics")
-            self.write(savefile, 'fenics')
+    #     elif(savefile[-4:] == 'xdmf'):
+    #         print("exporting to fenics")
+    #         self.write(savefile, 'fenics')
         
-        return Mesh(savefile)
+    #     return Mesh(savefile)
     
-    def setNameMesh(self,nameMesh):
-        nameMeshSplit = nameMesh.split('.')
-        self.format = nameMeshSplit[-1]
-        self.radFileMesh = reduce(lambda x,y : x + '.' + y, nameMeshSplit[:-1])
-        self.radFileMesh += ".{0}"
+    def setNameMesh(self, meshname):
+        self.radFileMesh,  self.format = meshname.split('.')
+        self.radFileMesh += '.{0}'
+        
+        
+# self.mesh = pygmsh.generate_mesh(self, verbose=False, dim=2, prune_vertices=True, prune_z_0=True,
+# remove_faces=False, extra_gmsh_arguments=gmsh_opt,  mesh_file_type='msh4') # it should be msh2 cause of tags
