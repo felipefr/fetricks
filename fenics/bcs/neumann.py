f2770c6ee39d6c905211bf2ef1fb4390c54ad78e
Felipe Rocha <felipemech@gmail.com>
2023-04-14 14:23:20 +0200

    temptative implementing cpp neumann condition (to solve the random bug with bcs and also to make it faster)

--- fenics/bcs/neumann.py
+++ fenics/bcs/neumann.py
@@ -16,6 +16,60 @@ Please report all bugs and problems to <felipe.figueredo-rocha@ec-nantes.fr>, or
 import dolfin as df
 import numpy as np
 
+
+        # cell = df.Cell(self.mesh, ufc_cell.index)
+        # n = cell.normal(ufc_cell.local_facet)
+        # values[:] = np.outer(self.g,n.array()[:self.mesh.geometric_dimension()]).flatten()
+
+code = """
+#include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+#include <dolfin/function/Expression.h>
+#include <dolfin/mesh/Cell.h>
+#include <dolfin/mesh/Mesh.h>
+#include <dolfin/geometry/Point.h>
+
+typedef Eigen::VectorXd npArray;
+typedef Eigen::VectorXi npArrayInt;
+typedef dolfin::Mesh dfMesh;
+
+class BoundarySource : public dolfin::Expression {
+  public:
+     
+    dfMesh mesh;
+    npArray gs;
+    int nc;
+    
+    BoundarySource(dfMesh m, npArray g, int nc) : dolfin::Expression(nc), mesh(mesh), gs(g), nc(nc) { }
+
+    void eval(Eigen::Ref<Eigen::VectorXd> values,
+                      Eigen::Ref<const Eigen::VectorXd> x,
+                      const ufc::cell& cell) const {
+        
+        dolfin::Cell cell_local(mesh, cell.index);
+        dolfin::Point normal = cell_local.normal(cell.local_facet);         
+        int gdim = gs.size();    
+    
+        for(int i = 0; i<gdim ; i++){ 
+            for(int j = 0; j<gdim ; j++) values[i*gdim + j] = normal.coordinates()[i]*gs[j]; 
+        }
+    }
+                      
+                    
+};
+
+PYBIND11_MODULE(SIGNATURE, m) {
+    pybind11::class_<BoundarySource, std::shared_ptr<BoundarySource>, dolfin::Expression>
+    (m, "BoundarySource")
+    .def(pybind11::init< dfMesh, npArray, int>())
+    .def("__call__", &BoundarySource::eval);
+}
+"""
+
+
+compCode = df.compile_cpp_code(code)
+BoundarySourceCpp = lambda gdim, mesh, g: df.CompiledExpression(compCode.BoundarySource(mesh, np.zeros(2), g, gdim*gdim))
+
 # buggy : sometimes crashes as eval is not available
 # imposes dot(sig,n) = g  
 class NeumannTensorSource(df.UserExpression):
@@ -25,10 +79,11 @@ class NeumannTensorSource(df.UserExpression):
         super().__init__(**kwargs)
         
     def eval_cell(self, values, x, ufc_cell):
-        cell = df.Cell(self.mesh, ufc_cell.index)
-        n = cell.normal(ufc_cell.local_facet)
-        values[:] = np.outer(self.g,n.array()[:self.mesh.geometric_dimension()]).flatten()
-        
+        if(ufc_cell.local_facet>-1):
+            cell = df.Cell(self.mesh, ufc_cell.index)
+            n = cell.normal(ufc_cell.local_facet)
+            values[:] = np.outer(self.g,n.array()[:self.mesh.geometric_dimension()]).flatten()
+           
     def value_shape(self):
         return (self.mesh.geometric_dimension(),self.mesh.geometric_dimension())
 
@@ -39,21 +94,29 @@ class NeumannVectorSource(df.UserExpression):
         self.g = g
         super().__init__(**kwargs)
     def eval_cell(self, values, x, ufc_cell):
-        cell = df.Cell(self.mesh, ufc_cell.index)
-        n = cell.normal(ufc_cell.local_facet)
-        values[:] = self.g*n.array()[:self.mesh.geometric_dimension()]
-        
+        if(ufc_cell.local_facet>-1):
+            cell = df.Cell(self.mesh, ufc_cell.index)
+            n = cell.normal(ufc_cell.local_facet)
+            values[:] = self.g*n.array()[:self.mesh.geometric_dimension()]
+            
     def value_shape(self):
         return (self.mesh.geometric_dimension(),)
     
 
-
+class SelectBoundary(df.SubDomain):
+      def inside(self, x, on_boundary):
+          return on_boundary 
+      
 def NeumannVectorBC(W, t, mesh, flag):
-    return [df.DirichletBC(W, NeumannTensorSource(mesh, t.values()) , mesh.boundaries, flag)]
+    g = NeumannTensorSource(mesh, t.values()) 
+    # g = df.interpolate(g, df.VectorFunctionSpace(mesh, "BDM", 2, restriction=SelectBoundary()))
+    # g = BoundarySourceCpp(2, mesh, t.values())
+    return [df.DirichletBC(W, g , mesh.boundaries, flag)]
 
 
 def NeumannBC(W, t, mesh, flag):
-    return [df.DirichletBC(W, NeumannVectorSource(mesh, t.values()) , mesh.boundaries, flag)]
+    g = NeumannVectorSource(mesh, t.values())
+    return [df.DirichletBC(W, g, mesh.boundaries, flag)]
 
 # This is Neumann but when normal are aligned with the cartesian axes
 def NeumannVectorBC_given_normal(W, t, normal, mesh, flag):
