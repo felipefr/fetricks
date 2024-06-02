aea21f8c5eb5bddce8a0fbf58acb712ee440c2e7
Felipe Rocha <f.rocha.felipe@gmail.com>
2024-06-03 01:11:24 +0200

    some updates while doing fetricksx

--- examples/truss/truss.py
+++ examples/truss/truss.py
@@ -1,97 +1,63 @@
+# Example truss
+
+import os, sys
+os.environ['HDF5_DISABLE_VERSION_CHECK']='2'
+sys.path.append("/home/felipe/sources/fetricks")
+
 import dolfin as df 
 import numpy as np
 
-import ddfenics.fenics.fenicsUtils as feut
-import ddfenics.fenics.wrapper_io as iofe
-# import ddfenics.core.fenics_tools.misc as feut
-import ddfenics.mechanics.misc as mech
-
-from ddfenics.dd.ddfunction import DDFunction
-
-from functools import partial
-from truss_utils import *
+import fetricks as ft
 
 
-def solve_truss(mesh, sigma_law):
-    Area = df.Constant(1.0) # assumes unitary area
-    
-    Lx, Ly = np.max(mesh.coordinates(), axis = 0)
-
-    t = getTangentTruss(mesh)
-    
-    grad_truss = partial(tgrad, t = t)
-    
-    Ue = df.VectorElement("CG", mesh.ufl_cell(), 1, dim=2)
-    Uh = df.FunctionSpace(mesh, Ue)
-
-    se = df.VectorElement("DG", mesh.ufl_cell(), 0, dim = 2)
-    sh0 = df.FunctionSpace(mesh, se)
- 
-    
-    def cornerLeftBottom(x, on_boundary):
-        return df.near(x[0], 0.0) and df.near(x[1], 0.0)
-
-    def cornerLeftTop(x, on_boundary):
-        return df.near(x[0], 0.0) and df.near(x[1], Ly)
-
-    bc1 = df.DirichletBC(Uh, df.Constant((0.0,0.0)), cornerLeftBottom, method = 'pointwise' )
-    bc2 = df.DirichletBC(Uh, df.Constant((0.0,0.0)), cornerLeftTop, method = 'pointwise')
-    
-    fy = -1.0
-    p1 = df.Point((0.5*Lx, 0.0))
-    p2 = df.Point((Lx, 0.0))             
-    
-    # # Define variational problem
-    uh = df.TrialFunction(Uh) 
-    vh = df.TestFunction(Uh)
-    
-    dx = df.Measure('dx', mesh)
-    
-    a = df.inner( Area*sigma_law(grad_truss(uh)),  grad_truss(vh))*dx
-    b = df.inner( df.Constant((0.,0.)) , vh)*dx
-    
-    A, b = df.assemble_system(a, b, [bc1, bc2])
-    
-    for p in [p1, p2]: 
-        delta = df.PointSource(Uh.sub(1), p, fy)
-        delta.apply(b) # Only in b (I don't why)
-
-    uh = df.Function(Uh)
-    
-    # Compute solution
-    df.solve(A, uh.vector(), b)   
-    
-
-    g = grad_truss(uh)
-
-    zh = DDFunction(sh0)
-    zh.update(df.as_tensor((g,sigma_law(g))))
-    
-    
-    return uh, zh
 
 if __name__ == '__main__':
     
-    mesh = getMeshTruss()
-    
-    E = 100.0
-    sigma_law = lambda e: df.Constant(E)*e 
-    
-    uh, zh = solve_truss(mesh, sigma_law )
+    L = 3.6 
+    X = np.array([[0., 0.],
+                    [L, 0.],
+                    [2*L, 0.],
+                    [0., L],
+                    [L, L],
+                    [2*L, L]])
 
-    # epsh.rename('eps', '')
-    zh.rename('z', '')
+    cells = np.array([  [0, 1],
+                        [1, 2],
+                        [3, 4],
+                        [4, 5],
+                        [2, 5],
+                        [1, 4],
+                        [1, 5],
+                        [2, 4],
+                        [0, 4],
+                        [1, 3]], dtype = int)
+
+    fy = -1.0
+    p1 = df.Point((0.0, 0.0)) # node 0
+    p2 = df.Point((0, L)) # node 3           
+    p3 = df.Point((L, 0.0)) # node 1
+    p4 = df.Point((2*L, 0.0)) # node 2           
+    E = 100.0
+    
+
+# for dirichlet and neumann: tuple of (node (int or df.Point), direction, value)
+    param = {
+    'dirichlet': [(p1, 0, 0. ), (p1, 1, 0. ),
+                  (p2, 0, 0. ), (p2, 1, 0. )],
+    'neumann': [(p3, 1 , fy), (p4, 1 , fy)], 
+    'sigma_law': lambda e: df.Constant(E)*e,
+    'Area': df.Constant(1.0), 
+    'X': X,
+    'cells': cells
+    }
+    
+    
+    uh = ft.solve_truss(param)
+    zh = ft.posproc_truss(param, uh)
+    
     uh.rename('u', '')
     
-    iofe.exportXDMF_gen("truss_vtk.xdmf", fields={'vertex': [uh], 'cell': [zh]})
+    ft.exportXDMF_gen("truss_vtk_2.xdmf", fields={'vertex': [uh], 'cell': [zh['strain'], zh['stress']]})
     
-    
-    print(np.min(uh.vector().get_local()), np.max(uh.vector().get_local()))
-    print(np.min(zh.data(), axis = 0))
-    print(np.max(zh.data(), axis = 0))
-    
-    np.savetxt('database_ref.txt', zh.data(), header = '1.0 \n%d 2 1 1'%zh.data().shape[0], comments = '', fmt='%.8e', )
-
-
 
 
